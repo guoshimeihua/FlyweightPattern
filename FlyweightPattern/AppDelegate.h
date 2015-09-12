bcde4f877bfdb68b1eae5716fb98308f8350ffd4
Bruce chen <cqdeyouxiang@163.com>
2015-09-12 14:38:31 +0800

    Initial Commit

---
+++ FlyweightPattern/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FlyweightPattern
+//
+//  Created by Bruce on 15/9/12.
+//  Copyright (c) 2015年 Bruce. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
